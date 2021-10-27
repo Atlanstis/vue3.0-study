4e3ac7f151a8d55b89a7722c0437fdc73f1d7c21
Gypsophila <futur_@163.com>
2021-10-27 17:47:57 +0800

    reactive 简易实现

--- README.md
+++ README.md
@@ -78,3 +78,22 @@ package 目录结构
 ## Composition API
 
 > [Composition API 案例](https://github.com/Atlanstis/vue3.0-study/tree/main/todolist)
+
+## 响应式系统
+
+### 回顾
+
+- Proxy 对象实现属性监听
+- 多层属性嵌套，在访问属性过程中处理下一级属性
+- 默认监听动态添加的属性
+- 默认监听属性的删除操作
+- 默认监听数组索引和 length 属性
+- 可以作为单独的模块使用
+
+### 实现
+
+#### reactive
+
+- 接受一个参数，判断这参数是否为对象（只能为对象）
+- 创建拦截器对象 handler，设置 get/set/deleteProperty
+- 返回 Proxy 对象
---
+++ reactivity/reactivity/index.js
@@ -0,0 +1,74 @@
+const isObject = (val) => val !== null && typeof val === 'object'
+const convert = (target) => (isObject(target) ? reactive(target) : target)
+const hasOwnProperty = Object.prototype.hasOwnProperty
+const hasOwn = (target, key) => hasOwnProperty.call(target, key)
+
+export function reactive(target) {
+  if (!isObject(target)) return target
+  const handler = {
+    get: (target, key, receiver) => {
+      // 收集依赖
+      track(target, key)
+      const result = Reflect.get(target, key, receiver)
+      return convert(result)
+    },
+
+    set: (target, key, value, receiver) => {
+      const oldVal = Reflect.get(target, key, receiver)
+      let result = true
+      if (oldVal !== value) {
+        result = Reflect.set(target, key, value, receiver)
+        // 触发更新
+        trigger(target, key)
+      }
+      return result
+    },
+
+    deleteProperty: (target, key) => {
+      const hadKey = hasOwn(target, key)
+      const result = Reflect.deleteProperty(target, key)
+      if (hadKey && result) {
+        // 触发更新
+        trigger(target, key)
+      }
+      return result
+    }
+  }
+  return new Proxy(target, handler)
+}
+
+let activeEffect = null
+
+export function effect(callback) {
+  activeEffect = callback
+  callback() // 访问响应式对象属性，去收集依赖
+  activeEffect = null
+}
+
+let targetMap = new WeakMap()
+
+// 依赖收集的函数
+export function track(target, key) {
+  if (!activeEffect) return
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()))
+  }
+  dep.add(activeEffect)
+}
+
+// 触发更新的函数
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target)
+  if (!depsMap) return
+  const dep = depsMap.get(key)
+  if (dep) {
+    dep.forEach((effect) => {
+      effect()
+    })
+  }
+}
